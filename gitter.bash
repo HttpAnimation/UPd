350f7d9a1824f98aa72bf03eca3c6879539de348
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-03-24 16:35:59 -0700

    Create gitter.bash

---
+++ gitter.bash
@@ -0,0 +1,10 @@
+git clone -b main https://github.com/HttpAnimation/UPd.git
+mv UPd main
+git clone -b gitter https://github.com/HttpAnimation/UPd.git
+mv UPd gitter
+git clone -b gh-pages https://github.com/HttpAnimation/UPd.git
+mv UPd gh-pages
+git clone -b docs https://github.com/HttpAnimation/UPd.git
+mv UPd docs
+git clone -b gitter https://github.com/HttpAnimation/UPd.git
+mv UPd gitter
